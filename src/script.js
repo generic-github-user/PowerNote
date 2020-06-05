0641b0c50f570ca23fa777767e530a8cc09ac032
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-06-05 18:39:36 -0400

    load test data

--- src/script.js
+++ src/script.js
@@ -1,1 +1,39 @@
+var testdata = '\
+inheritors\
+\nsolvency\
+\nmalfeasance\
+\n\
+\nconcomitant\
+\nglass castle\
+\ndiminutive\
+\ncommissary\
+\nlapidary\
+\ncatchetism\
+\nsurreptitious\
+\npreeminent\
+\ndissonance\
+\npretense\
+\nhaberdashery\
+\nfallow\
+\nlugubrious\
+\ncantankerous\
+\n\
+\nnon sequiturs\
+\naplomb\
+\nslovenly\
+\nsupple\
+\nvicariously\
+\nlaconic\
+\nattuned\
+\ntact\
+\nzany\
+\nmotile\
+\nexigence';
+
+notes = testdata.split('\n');
+notes.forEach((note) => {
+      item = $('<li type="button" class="list-group-item list-group-item-action">Lorem ipsum</li>');
+      item.text(note);
+      $('#notes-panel').append(item);
+})
 Sortable.create($('#notes-panel')[0]);
