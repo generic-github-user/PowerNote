4692c45fbf313623a2e1ff33edb31d8e783c6c24
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2020-06-05 23:56:37 -0400

    store all list events

--- src/script.js
+++ src/script.js
@@ -33,14 +33,31 @@ inheritors\
 function generate_id() {
       return Math.floor(Math.random() * 1e16).toString(16)
 }
+
+groups = {};
+event_chain = [];
+
+// sortable.on($('#notes-panel')[0],'onEnd',(e)=>{console.log(e)})
+// Sortable.utils.on($('#notes-panel').children()[0],'onStart',(events)=>{console.log(true)})
+
+function record_event(event) {
+      event_chain.push({
+            'time': new Date().getTime(),
+            'event': event
+      });
+}
+
 notes = testdata.split('\n');
 notes.forEach((note) => {
       item = $('<li type="button" class="list-group-item list-group-item-action">Lorem ipsum</li>');
       item.text(note);
       $('#notes-panel').append(item);
 })
-Sortable.create($('#notes-panel')[0], {
+sortable = Sortable.create($('#notes-panel')[0], {
       multiDrag: true,
       selectedClass: "selected",
-      animation: 150
+      animation: 150,
+      onStart: function(evt) {
+            record_event(evt)
+      }
 });
